d4e8e105926c38f157663db8a78361ad732d24b7
0x43ow <108293462+0x43ow@users.noreply.github.com>
2022-10-26 00:18:04 +0300

    Add files via upload

---
+++ CaptchaGUI/generator.py
@@ -0,0 +1,20 @@
+from captcha.image import ImageCaptcha
+from random import choice
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def get_random_string(length=int):
+    # Generates a variable length string consisting of 0-9 digits
+    str = ''
+    for i in range(length):
+        str += choice(numbers)
+    return str
+
+def get_captcha_image(length=int):
+    # Generates a captcha image and writes it on disk, returns its solution
+    image = ImageCaptcha(160, 60, fonts=['assets/fonts/OpenSans-Regular.ttf'])
+    s = get_random_string(length)
+    image.write(s, 'out.png')
+    return s
+
+get_captcha_image(4)
